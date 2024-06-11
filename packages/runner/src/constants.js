c635cb6a9e5c0f97d5f381c474635183961e4130
Torsten Dittmann <torsten.dittmann@googlemail.com>
2024-06-11 23:35:15 +0200

    fix: missing report directory to gitignore

--- packages/runner/src/constants.js
+++ packages/runner/src/constants.js
@@ -3,4 +3,5 @@ export const DEVICES = ["mobile", "desktop"];
 /** @type {Browsers[]} */
 export const BROWSERS = ["chromium", "webkit", "firefox"];
 export const GIT_IGNORE = `current/
-diffs/`;
+diffs/
+report/`;
